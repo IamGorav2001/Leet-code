39d2c05b51f3d23b5beecf47e677c571b4e39bd8
Gorav Singh <88999407+gorav-singh@users.noreply.github.com>
2022-12-30 12:27:08 +0530

    Added solution - LeetHub

--- Pattern 5 - GFG/pattern-5.cpp
+++ Pattern 5 - GFG/pattern-5.cpp
@@ -11,7 +11,7 @@ public:
 	    // code here
 	    for(int i=n;i>0;i--){
 	        for(int j=0;j<i;j++){
-	            cout<<"*"<<" ";
+	            cout<<"* ";
 	        }
 	        cout<<endl;
 	    }
